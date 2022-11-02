d26eefebd9551f5a8beb2aae90c48bf9ac5305e8
marcotuliovd <marcotuliodiniz2257@gmail.com>
2022-11-02 17:43:34 -0300

    middlewares all

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 const express = require('express');
+const validateCreatedAt = require('./middleware/validateCreatedAt');
 const validateDescription = require('./middleware/validateDescription');
+const validateDifficulty = require('./middleware/validateDifficulty');
 const validateName = require('./middleware/validateName');
 const validatePrice = require('./middleware/validatePrice');
+const validateRating = require('./middleware/validateRating');
 
 const app = express();
 
 app.use(express.json());
 
 app.post('/activities',
+    validateDifficulty,
+    validateRating,
+    validateCreatedAt,
     validateDescription,
     validateName,
     validatePrice,
